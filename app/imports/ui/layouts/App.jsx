d70c02bc7547a3b6d8da229fc1ebb711f4a3f29b
Jonathan-Ma <jma66@hawaii.edu>
2021-11-10 12:50:05 -1000

    OHA homepage

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -4,17 +4,8 @@ import { Meteor } from 'meteor/meteor';
 import 'semantic-ui-css/semantic.css';
 import { Roles } from 'meteor/alanning:roles';
 import { HashRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import NavBar from '../components/NavBar';
-import Footer from '../components/Footer';
 import Landing from '../pages/Landing';
-import ListStuff from '../pages/ListStuff';
-import ListStuffAdmin from '../pages/ListStuffAdmin';
-import AddStuff from '../pages/AddStuff';
-import EditStuff from '../pages/EditStuff';
 import NotFound from '../pages/NotFound';
-import Signin from '../pages/Signin';
-import Signup from '../pages/Signup';
-import Signout from '../pages/Signout';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 class App extends React.Component {
@@ -22,19 +13,10 @@ class App extends React.Component {
     return (
         <Router>
           <div>
-            <NavBar/>
             <Switch>
               <Route exact path="/" component={Landing}/>
-              <Route path="/signin" component={Signin}/>
-              <Route path="/signup" component={Signup}/>
-              <ProtectedRoute path="/list" component={ListStuff}/>
-              <ProtectedRoute path="/add" component={AddStuff}/>
-              <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
-              <AdminProtectedRoute path="/admin" component={ListStuffAdmin}/>
-              <ProtectedRoute path="/signout" component={Signout}/>
               <Route component={NotFound}/>
             </Switch>
-            <Footer/>
           </div>
         </Router>
     );
--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
-import { Grid, Image } from 'semantic-ui-react';
+import { Container, Grid, Image } from 'semantic-ui-react';
 
 /** A simple static component to render some text for the landing page. */
 class Landing extends React.Component {
   render() {
     return (
-        <Grid verticalAlign='middle' textAlign='center' container>
+        <div>
+          <div id={'nav'}>
+            <Container>
+              <Image floated='left' src={'../images/nav1.jpg'}/>
+            </Container>
+        </div>
+          <div id={'nav2'}>
+            <Container>
+              <Image fluid src={'../images/nav2.jpg'}/>
+            </Container>
+          </div>
+          <div id={'menu'}>
+            <Container>
+              <Grid verticalAlign='middle' textAlign='center' container>
+                <Grid.Row>
+                  <Image src={'../images/menu.jpg'}/>
+                </Grid.Row>
+                <Grid.Row>
+                  <Image src={'../images/2nd.jpg'}/>
+                </Grid.Row>
+                <Grid.Row>
+                  <Image src={'../images/menu2.jpg'}/>
+                </Grid.Row>
+                <Grid.Row>
+                  <Image src={'../images/menu3.jpg'}/>
+                </Grid.Row>
+              </Grid>
+            </Container>
+          </div>
+        </div>
 
-          <Grid.Column width={4}>
-            <Image size='small' circular src="/images/meteor-logo.png"/>
-          </Grid.Column>
-
-          <Grid.Column width={8}>
-            <h1>Welcome to this template</h1>
-            <p>Now get to work and modify this app!</p>
-          </Grid.Column>
-
-        </Grid>
     );
   }
 }
