7ba2b20c23a5631d38b2c230d749018e962c6ac6
Arslan Rakhmankulov <arslanr@hawaii.edu>
2021-11-14 22:58:44 -1000

    need to get script to work

--- app/imports/ui/pages/Landing.jsx
+++ app/imports/ui/pages/Landing.jsx
@@ -5,38 +5,88 @@ import { Container, Grid, Image } from 'semantic-ui-react';
 class Landing extends React.Component {
   render() {
     return (
-        <div>
-          <div id={'nav'}>
-            <Container>
-              <Image floated='left' src={'../images/nav1.jpg'}/>
-            </Container>
-        </div>
-          <div id={'nav2'}>
-            <Container>
-              <Image fluid src={'../images/nav2.jpg'}/>
-            </Container>
-          </div>
-          <div id={'menu'}>
-            <Container>
-              <Grid verticalAlign='middle' textAlign='center' container>
-                <Grid.Row>
-                  <Image src={'../images/menu.jpg'}/>
-                </Grid.Row>
-                <Grid.Row>
-                  <Image src={'../images/2nd.jpg'}/>
-                </Grid.Row>
-                <Grid.Row>
-                  <Image src={'../images/menu2.jpg'}/>
-                </Grid.Row>
-                <Grid.Row>
-                  <Image src={'../images/menu3.jpg'}/>
-                </Grid.Row>
-              </Grid>
-            </Container>
+      <div class="skw-pages">
+      <div class="skw-page skw-page-1 active">
+        <div class="skw-page__half skw-page__half--left">
+          <div class="skw-page__skewed">
+            <div class="skw-page__content"></div>
           </div>
         </div>
-
-    );
+        <div class="skw-page__half skw-page__half--right">
+          <div class="skw-page__skewed">
+            <div class="skw-page__content">
+              <h2 class="skw-page__heading">Skewed One Page Scroll</h2>
+              <p class="skw-page__description">Just scroll down</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="skw-page skw-page-2">
+        <div class="skw-page__half skw-page__half--left">
+          <div class="skw-page__skewed">
+            <div class="skw-page__content">
+              <h2 class="skw-page__heading">Page 2</h2>
+              <p class="skw-page__description">Nothing to do here, continue scrolling.</p>
+            </div>
+          </div>
+        </div>
+        <div class="skw-page__half skw-page__half--right">
+          <div class="skw-page__skewed">
+            <div class="skw-page__content"></div>
+          </div>
+        </div>
+      </div>
+      <div class="skw-page skw-page-3">
+        <div class="skw-page__half skw-page__half--left">
+          <div class="skw-page__skewed">
+            <div class="skw-page__content"></div>
+          </div>
+        </div>
+        <div class="skw-page__half skw-page__half--right">
+          <div class="skw-page__skewed">
+            <div class="skw-page__content">
+              <h2 class="skw-page__heading">Page 3</h2>
+              <p class="skw-page__description">The end is near, I promise!</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="skw-page skw-page-4">
+        <div class="skw-page__half skw-page__half--left">
+          <div class="skw-page__skewed">
+            <div class="skw-page__content">
+              <h2 class="skw-page__heading">Page 4</h2>
+              <p class="skw-page__description">Ok, ok, just one more scroll!</p>
+            </div>
+          </div>
+        </div>
+        <div class="skw-page__half skw-page__half--right">
+          <div class="skw-page__skewed">
+            <div class="skw-page__content"></div>
+          </div>
+        </div>
+      </div>
+      <div class="skw-page skw-page-5">
+        <div class="skw-page__half skw-page__half--left">
+          <div class="skw-page__skewed">
+            <div class="skw-page__content"></div>
+          </div>
+        </div>
+        <div class="skw-page__half skw-page__half--right">
+          <div class="skw-page__skewed">
+            <div class="skw-page__content">
+              <h2 class="skw-page__heading">Epic finale</h2>
+              <p class="skw-page__description">
+                Feel free to check 
+                <a class="skw-page__link" href="https://codepen.io/suez/pens/public/" target="_blank">my other pens</a> and follow me on 
+                <a class="skw-page__link" href="https://twitter.com/NikolayTalanov" target="_blank">Twitter</a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    )
   }
 }
 
